227a9c2e7bf55cb59bedff13e5ed295ab878dfbf
Mateusz Tokarski <matitzlol@gmail.com>
2023-12-02 19:45:19 +0100

    Initial commit

---
+++ D1/index.js
@@ -0,0 +1,39 @@
+import fs from "fs";
+function partOne(fName) {
+  const lines = fs.readFileSync(fName, "utf-8").trim().split("\r\n");
+  const values = lines.map((line) => {
+    let fDigit = line.split("").find((v) => !Number.isNaN(Number(v)));
+    let lDigit = line.split("").findLast((v) => !Number.isNaN(Number(v)));
+    return Number(fDigit + lDigit);
+  });
+  console.log(values);
+  return values.reduce((s, v) => s + v);
+}
+//console.log(partOne("./ex.txt"));
+console.log(partOne("./input.txt"));
+
+const mappings = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+// function partTwo(fName) {
+//   const lines = fs.readFileSync(fName, "utf-8").trim().split("\r\n");
+//   const values = lines.map((line) => {
+//     let fDigit = line.split("").find((v) => !Number.isNaN(Number(v)));
+
+//     let lDigit = line.split("").findLast((v) => !Number.isNaN(Number(v)));
+
+//     return Number(fDigit + lDigit);
+//   });
+//   console.log(values);
+//   return values.reduce((s, v) => s + v);
+// }
+// console.log(partTwo("./input.txt"));
