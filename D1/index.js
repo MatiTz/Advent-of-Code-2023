4a626656b015241235964c01f27b64b881422f18
Mateusz Tokarski <matitzlol@gmail.com>
2023-12-02 19:47:38 +0100

    part 1

--- D1/index.js
+++ D1/index.js
@@ -12,6 +12,8 @@ function partOne(fName) {
 //console.log(partOne("./ex.txt"));
 console.log(partOne("./input.txt"));
 
+// PART 2
+
 const mappings = [
   "one",
   "two",
@@ -23,17 +25,3 @@ const mappings = [
   "eight",
   "nine",
 ];
-
-// function partTwo(fName) {
-//   const lines = fs.readFileSync(fName, "utf-8").trim().split("\r\n");
-//   const values = lines.map((line) => {
-//     let fDigit = line.split("").find((v) => !Number.isNaN(Number(v)));
-
-//     let lDigit = line.split("").findLast((v) => !Number.isNaN(Number(v)));
-
-//     return Number(fDigit + lDigit);
-//   });
-//   console.log(values);
-//   return values.reduce((s, v) => s + v);
-// }
-// console.log(partTwo("./input.txt"));
